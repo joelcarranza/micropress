24c84ddbe52259b5a7d1ed470b601af98a455ff6
Joel Carranza <joel.carranza@gmail.com>
2011-05-28 17:21:32 -0700

    initial reorg checkin

---
+++ micropress/main.py
@@ -0,0 +1,23 @@
+from micropress import Site,SITE_CONFIG_PATH
+import sys
+
+if __name__ == '__main__':
+  if len(sys.argv) <= 1:
+    cmd = 'brew' 
+  else:
+    cmd = sys.argv[1]
+  site = Site(SITE_CONFIG_PATH)
+  if cmd == 'brew':
+    site.brew()
+  elif cmd == 'run':
+    # web.py -  If called from the command line, it will start an HTTP server 
+    # on the port named in the first command line argument, or, if there is no
+    # argument, on port 8080.
+    sys.argv = sys.argv[2:]
+    site.run()
+  elif cmd == 'clean':
+    site.clean()
+  elif cmd == 'inventory':
+    site.inventory()
+  else:
+    raise Exception("Invalid command %s"  % cmd)
